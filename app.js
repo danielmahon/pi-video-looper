43d5a8ef1ed8762e6ad4c950ab4e70626bf3fec2
Daniel Mahon <daniel@mahonstudios.com>
2015-03-10 23:37:10 +0000

    working loop and sync

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var dotenv = require('dotenv').load();
 
-var omx = require('omxdirector').enableNativeLoop();
+var _ = require('underscore');
+var omx = require('omx-manager');
 var Download = require('download');
 var progress = require('download-status');
 var AWS = require('aws-sdk');
@@ -20,19 +21,14 @@ s3.listObjects(params, function(err, data) {
     return console.log(err, err.stack);
   }
 
-  console.log('Checking media');
-  var toDownload = [];
+  console.log('Syncing media');
+
+  var remoteFiles = [];
+  var localFiles = [];
 
   if (data.Contents.length) {
     data.Contents.forEach(function(file) {
-      if (file.Size) {
-        // Check for local versions
-        try {
-          fs.statSync(__dirname + '/media/' + file.Key.split('/').pop());
-        } catch(err) {
-          toDownload.push(file.Key);
-        }
-      }
+      if (file.Size) remoteFiles.push(file.Key.split('/').pop());
     });
   }
 
@@ -41,18 +37,29 @@ s3.listObjects(params, function(err, data) {
     console.log('Playing media in loop');
     fs.readdir(__dirname + '/media', function(err, files) {
       if (err) throw err;
-      omx.setVideoDir(__dirname + '/media');
-      omx.play(files, {loop: true, audioOutput: 'hdmi'});
+      localFiles = files;
+      // omx.enableMultipleNativeLoop();
+      omx.setVideosDirectory(__dirname + '/media');
+      console.log(localFiles);
+      omx.play(localFiles, {'--loop': false, '-b': true, '--no-osd': true, '-o': 'hdmi'}, true);
     });
   }
 
-  if (toDownload.length) {
+
+  if (remoteFiles.length) {
+    // Delete old videos
+    console.log('Clean up old videos...');
+    localFiles = fs.readdirSync(__dirname + '/media');
+    _.each(_.difference(localFiles, remoteFiles), function(fileToDelete) {
+      console.log('Deleting ' + fileToDelete);
+      fs.unlinkSync(__dirname + '/media/' + fileToDelete);
+    });
     // Download local versions if missing
     var download = new Download();
-    var endpoint = 'http://' + process.env.AWS_BUCKET + '.' + s3.endpoint.hostname + '/';
-    toDownload.forEach(function(path) {
-      console.log('Adding ' + path + ' to download queue');
-      download.get(endpoint + path);
+    var endpoint = 'http://' + process.env.AWS_BUCKET + '.' + s3.endpoint.hostname + '/' + data.Prefix + '/';
+    _.each(_.difference(remoteFiles, localFiles), function(fileToDownload) {
+      console.log('Adding ' + fileToDownload + ' to download queue');
+      download.get(endpoint + fileToDownload);
     });
     console.log('Downloading...');
     download.dest(__dirname + '/media/');
